a8a5d6d64041642f3947103008a07823d2581e22
Mahmoud Hosni <mah1998mod@gmail.com>
2023-01-21 14:47:03 +0200

    added get all and get by id for movies

--- MoviesAPI/MoviesAPI/Controllers/MoviesController.cs
+++ MoviesAPI/MoviesAPI/Controllers/MoviesController.cs
@@ -10,12 +10,59 @@ namespace MoviesAPI.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        private new List<string> _allowedExtentions = new List<string>() {".png",".jpg",".jfif" };
+        private new List<string> _allowedExtentions = new List<string>() { ".png", ".jpg", ".jfif" };
         private long _maxAllowedPosterSize = 1048576;
 
         public MoviesController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var movies = await _context.Movies
+                .OrderByDescending(m => m.Rate)
+                .Include(m => m.Genre)
+                .Select(m => new MovieDetailsDto
+                {
+                    Id = m.Id,
+                    Title = m.Title,
+                    Rate = m.Rate,
+                    Poster = m.Poster,
+                    GenreId = m.GenreId,
+                    StoreLine = m.StoreLine,
+                    Year = m.Year,
+                    GenreName = m.Genre.Name
+                })
+                .ToListAsync();
+
+            return Ok(movies);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var movie = await _context.Movies
+                .Include(m => m.Genre)
+                .SingleOrDefaultAsync(m => m.Id ==id);
+
+            if(movie == null)
+                return NotFound();
+
+            var dto = new MovieDetailsDto()
+            {
+                Id = movie.Id,
+                Title = movie.Title,
+                Rate = movie.Rate,
+                Poster = movie.Poster,
+                GenreId = movie.GenreId,
+                StoreLine = movie.StoreLine,
+                Year = movie.Year,
+                GenreName = movie.Genre.Name
+            };
+
+            return Ok(dto);
         }
 
         [HttpPost]
@@ -27,7 +74,7 @@ namespace MoviesAPI.Controllers
             if (input.Poster.Length > _maxAllowedPosterSize)
                 return BadRequest("Max allowed size for poster is 1MB!");
 
-            var isValidGenre = await _context.Genres.AnyAsync(g =>g.Id == input.GenreId);
+            var isValidGenre = await _context.Genres.AnyAsync(g => g.Id == input.GenreId);
             if (!isValidGenre)
                 return BadRequest("Invalid genre ID!");
 
